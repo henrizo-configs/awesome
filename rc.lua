97de456df66051a344f3068ff495b19381beea86
Matheus <matheus.hs97@gmail.com>
2019-07-17 10:43:54 -0300

    added startup programs

--- rc.lua
+++ rc.lua
@@ -575,3 +575,15 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Startup programs
+autorun = true
+autorunApps =
+{
+   "qjackctl",
+}
+if autorun then
+   for app = 1, #autorunApps do
+       awful.util.spawn(autorunApps[app])
+   end
+end
